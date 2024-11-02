860572efc598532a8ff6250167853c1f58cc39ad
RotierenderRoland <backfisch3000@googlemail.com>
2024-11-02 14:56:01 +0100

    added bundles to the groups in groups.py

--- bundlewrap_repo/groups.py
+++ bundlewrap_repo/groups.py
@@ -1,11 +1,11 @@
 groups = {
     'apache-webserver': {
-        'bundles': ["bundle1", "bundle2"],
+        'bundles': ["apache"],
         'member_patterns': [r"webserver-\d+"],
         'os': 'linux',
     },
     'loadbalancer': {
-        'bundles': ["bundle1", "bundle2"],
+        'bundles': ["ha_proxy"],
         'member_patterns': [r"loadbalancer-\d+"],
         'os': 'linux',
     },
